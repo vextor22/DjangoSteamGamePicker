6828205770cfabea84c12162ceb60b0a80972f4d
Matt Higgins <mrhiggi@g.clemson.edu>
2018-04-18 02:54:41 +0000

    modified static root for nginx

--- steamsite/steamsite/settings.py
+++ steamsite/steamsite/settings.py
@@ -116,5 +116,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
+STATIC_ROOT='/var/www/static'
